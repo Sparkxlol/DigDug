083d86021cfa292c24faa8d7d9ec1bd7a44fa5f7
Owen Keeler <22OMK01@stu.cpsk12.org>
2022-02-09 10:05:15 -0600

    changed window size

--- DigDug/DigDug.cpp
+++ DigDug/DigDug.cpp
@@ -6,7 +6,9 @@ int main()
     sf::RenderWindow window(sf::VideoMode(14 * 16, 15 * 16), "SFML works!");
     Spritesheet spriteSheet("Images/digDugSpritesheet.png", sf::Vector2i(256, 80), sf::Vector2i(16, 16));
     spriteSheet.setPosition(100, 100);
-    spriteSheet.loadSprite(3);
+    spriteSheet.loadSprite(13);
+
+    window.setSize(sf::Vector2u(1080, 1080));
 
     while (window.isOpen())
     {
