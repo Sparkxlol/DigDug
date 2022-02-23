75dda8edb0a235e7f9270e4df8e774f53d84fb09
Sparkx <77602232+Sparkxlol@users.noreply.github.com>
2022-02-23 08:58:12 -0600

    changed sand a bit

--- DigDug/Sand.cpp
+++ DigDug/Sand.cpp
@@ -14,48 +14,60 @@ Sand::~Sand()
 Sand::Sand(Spritesheet* fore, Spritesheet* back1, Spritesheet* back2, int initialFore, int initialBack, sf::Vector2f pos, sf::RenderWindow* win)
 	: foreground(fore), background(back1), background2(back2), window(win)
 {
+	// Moves each spritesheet to initial location.
 	foreground->setPosition(pos);
 	background->setPosition(pos);
 	background2->setPosition(pos);
 
 	// Sets correct sprites to foreground and background.
-	// Sets x and y mask / direction to corresponding foreground.
-	foreground->loadSprite(15);
+	foreground->loadSprite(initialFore);
 	background->loadSprite(initialBack);
 	background2->loadSprite(initialBack);
 
+	// Allow for correct positioning on the mask/texture.
 	initialBackground = initialBack;
 	initialForeground = initialFore;
 }
 
 
+// Changes the top background mask to the correct size based on current mask and inputted position/direction.
+// Moves this mask into the correct position and chooses the correct foreground sprite for the "path".
 void Sand::changeSand(sf::Vector2f playerPos, int dir)
 {
-	// Change x and y mask to correct values based on inputted direction and mask
-	// Use mask values to edit spritesheet texture rect.
-
+	// Finds x and y change from the player to the top left of the
+	// sand object and adds 16 to prevent negatives -> 32.
 	int xChange = playerPos.x - background->getPosition().x + 16;
 	int yChange = playerPos.y - background->getPosition().y + 16;
 
 	switch (dir)
 	{
 	case 0:
-		// If moving up and pos is greater than topMask and less than bottomMask
-		// increase bottomMask
+		// If not moving up in the top part and not fewer than current
+		// change, make bottomMask either current size or new change from bottom.
 		if (yChange > topMask && 32 - yChange > bottomMask)
 			bottomMask = (bottomMask > 32 - yChange) ? bottomMask : 32 - yChange; // Check if this is necessary...
 		break;
 	case 1:
-		// If moving down and pos is less than bottomMask and greater than topMask
-		// increase topMask
+		// If not moving down at the bottom and not fewer than current
+		// change, make topMask either the current yChange or the current topMask.
 		if (yChange < 32 - bottomMask && yChange > topMask)
 			topMask = (topMask > yChange) ? topMask : yChange;
 		break;
+	case 2:
+		// Works same as bottomMask case, just with rightMask instead.
+		if (xChange > leftMask && 32 - xChange > rightMask)
+			rightMask = (rightMask > 32 - xChange) ? rightMask : 32 - xChange;
+		break;
+	case 3:
+		// Works same as topMask case, just with leftMask instead.
+		if (xChange < 32 - rightMask && xChange > leftMask)	
+			leftMask = (leftMask > xChange) ? leftMask : xChange;
+		break;
 	}
 
-	//std::cout << bottomMask << " " << topMask << std::endl;
-
 	/*
+	// Finds out which sprite to load based on the current corresponding masking.
+	// Should be optimized, probably >.<
 	if (topMask > 0 && bottomMask > 0 && leftMask > 0 && rightMask > 0)
 		foreground->loadSprite(15);
 	else if (topMask > 0 && bottomMask > 0 && leftMask > 0)
@@ -80,25 +92,36 @@ void Sand::changeSand(sf::Vector2f playerPos, int dir)
 		foreground->loadSprite(5);
 	*/
 
+	// Make sure that the current height and width is not greater than 16.
 	int height = (topMask + bottomMask > 16) ? 0 : 16 - topMask - bottomMask;
+	int width = (leftMask + rightMask > 16) ? 0 : 16 - leftMask - rigthMask;
 
+	// Sets the top and the left of the mask to the current sprite plus the mask.
+	// Sets the width and height to the total masking or 16.
 	background2->setTextureRect(sf::IntRect(
-		initialBackground * 16,
+		initialBackground * 16 + leftMask,
 		initialBackground * 16 + topMask,
-		16,
+		width,
 		height
 	));
 
+	// Moves the sprite so the increase doesn't always come from the bottom of the mask when
+	// the topMask is changed instead of the bottomMask.
 	background2->setPosition(
 		sf::Vector2f(background->getPosition().x + leftMask, background->getPosition().y + topMask)
 	);
 }
 
 
-void Sand::reset(int spriteIndex, int initialFore, int initialBack)
+void Sand::reset(int spriteIndex, sf::Vector2f pos, int initialFore, int initialBack)
 {
 	// Resets the values of sand to load for the next level. 
 	// Sprite index is for color changes.
+	
+	background2->setPosition(pos);
+	background2->setTextureRect(sf::IntRect(initialFore, initialBack, 16, 16);
+	background2->loadSprite(spriteIndex);
+	background->loadSprite(spriteIndex);
 }
 
 
